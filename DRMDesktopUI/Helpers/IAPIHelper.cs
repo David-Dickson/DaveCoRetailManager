bcf2eccda69b6a798adad03d7e064ed93aa9625b
David Dickson <david.k.dickson1@gmail.com>
2022-10-28 08:24:12 -0400

    Wire Up Login Form to API
    Connected login form button to the
    authentication API endpint (/Token). Got
    back the bearer token or an exception if failed.

---
+++ DRMDesktopUI/Helpers/IAPIHelper.cs
@@ -0,0 +1,10 @@
+﻿using DRMDesktopUI.Models;
+using System.Threading.Tasks;
+
+namespace DRMDesktopUI.Helpers
+{
+    public interface IAPIHelper
+    {
+        Task<AuthenticatedUser> Authenticate(string username, string password);
+    }
+}
